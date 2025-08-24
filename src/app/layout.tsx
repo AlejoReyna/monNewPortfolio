6444ff01a182ae5897418fd65ed1804517d0f921
Alexis Reyna <alexis.rs@inverater.com>
2025-08-23 19:57:20 -0600

    commented footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -68,7 +68,7 @@ export default function RootLayout({
               <a id="top" />
               <Navbar />
               {children}
-              <Footer />
+              {/* <Footer /> */}
             </LanguageFade>
           </NavigationProvider>
         </LanguageProvider>
